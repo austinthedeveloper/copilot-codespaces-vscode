4ba029af735f5dfed7956ddfec5c417075002b6a
Austin Stewart <hello@austinthedeveloper.com>
2023-07-31 04:22:58 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills
+    };
+    // Write code here
+    // member.skills = skills;
+    return member;
+}
